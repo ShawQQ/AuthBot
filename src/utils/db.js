60d1f60c0f8bd2bbdcb77ff83ca4d705dad0a1c5
matias <matiaslarizza@gmail.com>
2021-10-16 17:32:31 +0200

    connectionString

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,11 +6,10 @@ class Database{
 	constructor(){
 		this._connected = false;
 		this._client = new Client({
-			user: process.env.DB_USER,
-			host: process.env.DB_HOST,
-			database: process.env.DB_NAME,
-			password: process.env.DB_PASSWORD,
-			port: process.env.DB_PORT
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+				rejectUnauthorized: !process.env.SANDBOX
+			}
 		});
 	}
 
