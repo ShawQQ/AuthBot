ea1d4e9f8bd1a90419adaa17c95876db17c5c598
matias <matiaslarizza@gmail.com>
2021-10-16 17:32:47 +0200

    create base table

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 require('dotenv').config();
-require('./src/utils/route').setRoute();
+const { Database } = require('./src/utils/db');
+const db = new Database();
+db.open().then(async () => {
+	await db.createBaseTable();
+	await db.close();
+	require('./src/utils/route').setRoute();
+});
