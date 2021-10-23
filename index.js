84e39a38bfe79cefd9a8a9ce48f3edf0a4b216c8
matias <matiaslarizza@gmail.com>
2021-10-23 22:47:29 +0200

    autoban

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 require('dotenv').config();
 const { Database } = require('./src/utils/db');
 const constants = require('./src/utils/constant');
+const twitch = require('./src/twitch/app');
 const db = new Database();
 db.open().then(async () => {
 	await db.createBaseTable();
 	await db.close();
 	require('./src/utils/route').setRoute();
 });
-console.log(constants.twitch.adminOauthUrl);
+
+//AUTOBAN
+/*
+db.open().then(async () => {
+	let users = await twitch.getCurrentSubs();
+	let currentUser = await db.getUsers();
+	let toBan = [];
+	
+	for(const current of currentUser){
+		let ban = true;
+		for(const user of users){
+			if(user.id == current.twitch_id){
+				ban = false;
+				break;
+			}
+		}
+		if(ban){
+			toBan.push(current.telegram_id);
+		}
+	}
+	console.log(toBan);
+});
+*/
