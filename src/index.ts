53b86e52630570ef0469c14b3534638d15d55cbe
matias <matiaslarizza@gmail.com>
2022-06-12 18:52:33 +0200

    crons

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { setWebHook, setRoute } from "./routing/router";
 import { DatabaseFactory } from "./utils/database/db";
+import { Logger } from 'tslog';
+import { TelegramCron } from './utils/cron/telegramCron';
+const CronJob = require('cron').CronJob;
 const db = DatabaseFactory.getDatabase();
+const log: Logger = new Logger();
 /**
  * Start the application
  */
@@ -9,6 +13,25 @@ async function start() {
   await db.createBaseTable();
   await setWebHook();
   setRoute();
+  const cron = new TelegramCron();
+  new CronJob(
+	'0 0 1 * *',
+	cron.banWarning,
+	null,
+	true,
+	'Europe/Rome'
+  );
+  new CronJob(
+	'0 0 2 * *',
+	cron.autoban,
+	null,
+	true,
+	'Europe/Rome'
+  );
 }
 
-start();
+try{
+	start();
+}catch(e){
+	log.error("Bot error", e);
+}
